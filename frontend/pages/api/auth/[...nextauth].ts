2841ac7cd557270a552a9c3eb4d1b3c1a91e7d46
Konstantin Ponomarev <ponomarevkonst@gmail.com>
2021-04-18 01:45:48 +0300

    next-auth first iter

---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,87 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import axios from "axios";
+import { User } from "next-auth";
+
+export interface AuthenticatedUser extends User {
+    accessToken?: string,
+    refreshToken?: string,
+}
+
+const settings = {
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            console.log("SIGNIN:", user, account, profile)
+            return true;
+        },
+        async redirect(url, baseUrl) {
+            console.log("REDIRECT:", url, baseUrl)
+            return baseUrl
+        },
+        async session(session, user) {
+            console.log("SESSION:", session, user)
+            return session
+        },
+        async jwt(token, user, account, profile, isNewUser) {
+            console.log("JWT:", token, user, account, profile, isNewUser)
+            return token
+        }
+    }
+}
+
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, settings);
+
+
+/*
+async signIn(user: AuthenticatedUser, account, profile?) {
+            // may have to switch it up a bit for other providers
+            if (account.provider === "github") {
+                // extract these two tokens
+                const { accessToken, idToken } = account;
+
+                // make a POST request to the DRF backend
+                try {
+                    const response = await axios.post(
+                        // tip: use a seperate .ts file or json file to store such URL endpoints
+                        "http://127.0.0.1:8000/api/social/login/google/",
+                        {
+                            access_token: accessToken, // note the differences in key and value variable names
+                            id_token: idToken,
+                        },
+                    );
+
+                    // extract the returned token from the DRF backend and add it to the `user` object
+                    const { access_token } = response.data;
+                    user.accessToken = access_token;
+
+                    return true; // return true if everything went well
+                } catch (error) {
+                    return false;
+                }
+            }
+            return false;
+        },
+
+        async jwt(token, user: AuthenticatedUser, account, profile, isNewUser) {
+            if (user) {
+                const { accessToken } = user;
+
+                // reform the `token` object from the access token we appended to the `user` object
+                token.accessToken = accessToken;
+            }
+
+            return token;
+        },
+
+        async session(session, user: AuthenticatedUser) {
+            session.accessToken = user.accessToken;
+            return session;
+        },
+ */
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,36 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import React from "react";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
-  return <Title>My page</Title>
+  const [session, loading] = useSession();
+  return (
+      <div className={styles.container}>
+        <Head>
+          <title>Auth Examples</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          {!session && (
+              <>
+                Not signed in <br />
+                <button onClick={signIn}>Sign In</button>
+              </>
+          )}
+          {session && (
+              <>
+                Signed in as {session.user.email} <br />
+                <div>You can now access our super secret pages</div>
+                <button>
+                  <Link href="/secret">To the secret</Link>
+                </button>
+                <button onClick={signOut}>sign out</button>
+              </>
+          )}
+        </main>
+      </div>
+  );
 }
