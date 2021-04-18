8e9efa5188c50a9fa87f1212b950c9f74e7a2048
Kostuxa <samsonov.k.m@gmail.com>
2021-04-18 16:22:32 +0300

    companies

--- volunteer/companies/models.py
+++ volunteer/companies/models.py
@@ -2,8 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-class District(models.Model):
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField()
+    active = models.BooleanField(default=True)
+
+
+class ElectionType(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
 
+
+class Election(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    election_date = models.DateField(auto_now=False)
+    type = models.ForeignKey(ElectionType, on_delete=models.CASCADE)
+
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+
+class District(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    info = models.TextField(blank=True, null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
 class Company(models.Model):
-    pass
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True, null=True)
+
