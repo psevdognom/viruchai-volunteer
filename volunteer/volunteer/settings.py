21aabc0746adc7ca03d3d31155b2f708e2e86a5e
Konstantin Ponomarev <ponomarevkonst@gmail.com>
2021-04-26 03:33:30 +0300

    add swagger and small fixes

--- volunteer/volunteer/settings.py
+++ volunteer/volunteer/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'authentication',
     'companies',
     'tasks',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
+    'EXCEPTION_HANDLER': 'volunteer.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'authentication.backends.JWTAuthentication',
--- volunteer/volunteer/urls.py
+++ volunteer/volunteer/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Выручай-волонтер DRF API',
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authentication/', include('authentication.urls'))
+    path('authentication/', include('authentication.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0))
 ]
